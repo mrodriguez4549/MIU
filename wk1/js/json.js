555bfefdb964f95e93c4285672ef3e8e31b4e55c
mrodriguez4549 <mister4549@fullsail.edu>
2012-06-26 12:07:59 -0500

    creating first files

---
+++ wk1/js/json.js
@@ -0,0 +1,56 @@
+/* 
+	Marco Rodriguez -->
+  	06-21-12 -->
+ 	Project 4 -->
+	VFW 1206 -- >
+*/
+
+// JSON file 
+
+var json = {
+	"contact1": {
+		"fname": 		["First Name:", "Mike"],
+		"lname": 		["Last Name:", "Cook"],
+		"email": 		["Email:", "mcook@gmail.com"],
+		"date": 		["Date:", "2012-06-18"],
+		"make": 		["Make:", "Chevrolet"],
+		"car": 			["Car:", "Corvette"],
+		"caramount": 	["How many cars do you own:", "2"],
+		"fastest": 		["Fastest Time:", "12's"],
+		"hp": 			["Horse Power:", "400HP"],
+		"adder": 		["Power Adder:", "Nitrous Stage 1"],
+		"tranny": 		["Transmission:", "Manual"],
+		"ltech": 		["Referred from LS1Tech.com:", "Yes"],
+		"comments": 	["Any Comments:", "You guys Rock!"]
+	},
+	"contact2": {
+		"fname": 		["First Name:", "Albert"],
+		"lname": 		["Last Name:", "Bing"],
+		"email": 		["Email:", "abing@gmail.com"],
+		"date": 		["Date:", "2012-06-16"],
+		"make": 		["Make:", "BMW"],
+		"car": 			["Car:", "650ci"],
+		"caramount": 	["How many cars do you own:", "2"],
+		"fastest": 		["Fastest Time:", "12's"],
+		"hp": 			["Horse Power:", "400HP"],
+		"adder": 		["Power Adder:", "Single Turbo"],
+		"tranny": 		["Transmission:", "Auto"],
+		"ltech": 		["Referred from LS1Tech.com:", "No"],
+		"comments": 	["Any Comments:", "No thanks!"]
+	},
+	"contact3": {
+		"fname": 		["First Name:", "Gregg"],
+		"lname": 		["Last Name:", "Full"],
+		"email": 		["Email:", "gregg_full2899@gmail.com"],
+		"date": 		["Date:", "2012-05-13"],
+		"make": 		["Make:", "Ford"],
+		"car": 			["Car:", "Mustang Type-R"],
+		"caramount": 	["How many cars do you own:", "4"],
+		"fastest": 		["Fastest Time:", "10's"],
+		"hp": 			["Horse Power:", "600HP"],
+		"adder": 		["Any Power Adders:", "Nitrous Stage 3"],
+		"tranny": 		["Transmission:", "Manual"],
+		"ltech": 		["Referred from LS1Tech.com:", "Yes"],
+		"comments": 	["Any Comments:", "Sweet app, can't wait to try it out!"]
+	}
+}
